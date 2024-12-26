f0b955e646baaf454977484040da5b547eb2829e
Adrian Garza <adriangarza236@yahoo.com>
2024-12-26 14:40:27 -0600

    completed static lab

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    const firstLetter = string.charAt(0).toUpperCase()
+    const theRest = string.slice(1)
+    return firstLetter + theRest
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const words = string.split(" ");
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"].includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
+
 }
